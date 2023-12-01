799f160585e05cc4a104e1a0a8883a44aa593555
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2023-12-01 22:15:14 +0800

    Time: 2 ms (100.00%), Space: 43.7 MB (42.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,72 +1,28 @@
 class Solution {
-    private static final int M = 1000;
-    private static final int D = 500;
-    private static final int C = 100;
-    private static final int L = 50;
-    private static final int X = 10;
-    private static final int V = 5;
-    private static final int I = 1;
-    public static int romanToInt(String s) {
-        int value = 0;
-        int temp = 0;
+    public int romanToInt(String s) {
+    
+        int answer = 0, number = 0, prev = 0;
 
-        char[] letters = String.valueOf(s).toCharArray();
-
-        for (char a : letters){
-            switch (a){
-                case 'M' -> {
-                    if (temp == C) value += M - temp - temp;
-                    else {
-                        value += M;
-                    }
-                    temp = 0;
-                }
-                case 'D' -> {
-                    if (temp == C) value += D - temp - temp;
-                    else {
-                        value += D;
-                    }
-                    temp = 0;
-                }
-                case 'C' -> {
-                    if (temp == X) {
-                        value += C - temp - temp;
-                        temp = 0;
-                    }
-                    else {
-                        value += C;
-                        temp = C;
-                    }
-                }
-                case 'L' -> {
-                    if (temp == X) {
-                        value += L - temp - temp;
-                    }
-                    else {
-                        value += L;
-                    }
-                    temp = 0;
-                }
-                case 'X' -> {
-                    if (temp == I) value += X - temp - temp;
-                    else {
-                        value += X;
-                        temp = X;
-                    }
-                }
-                case 'V' -> {
-                    if (temp == I) value += V - temp - temp;
-                     else {
-                         value += V;
-                    }
-                    temp = 0;
-                }
-                case 'I' -> {
-                    value += 1;
-                    temp = I;
-                }
+        for (int j = s.length() - 1; j >= 0; j--) {
+            switch (s.charAt(j)) {
+                case 'M' -> number = 1000;
+                case 'D' -> number = 500;
+                case 'C' -> number = 100;
+                case 'L' -> number = 50;
+                case 'X' -> number = 10;
+                case 'V' -> number = 5;
+                case 'I' -> number = 1;
             }
+            
+            if (number < prev) {
+                answer -= number;
+            }
+            else {
+                answer += number;
+            }
+            
+            prev = number;
         }
-        return value;
+        return answer;
     }
 }
