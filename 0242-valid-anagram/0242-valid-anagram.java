1d30ff0dca4ad3757a2f3218fd1656392065b1d1
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-02-11 14:28:48 +0800

    Time: 4 ms (68.54%), Space: 44.4 MB (32.08%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
+    }
+}
