9da37300754bf6274e0e92484ef36bd9648aec99
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-02-11 20:53:05 +0800

    Time: 2 ms (14.82%), Space: 41.7 MB (48.43%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
     public static int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
+        String[] words = s.split("\\s+");
         return words[words.length - 1].length();
     }
 }
