ec1d4ca4992073152175eb38054c8162b98b7b13
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-02-29 01:26:31 +0800

    Time: 235 ms (15.57%), Space: 50.7 MB (58.64%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,32 @@
+class MyHashSet {
+    ArrayList<Integer> array = null;
+    public MyHashSet() {
+        array = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        array.add(key);
+    }
+    
+    public void remove(int key) {
+        Iterator<Integer> iterator = array.iterator();
+        while(iterator.hasNext()){
+            Integer a = iterator.next();
+            if (a == key){
+                iterator.remove();
+            }
+        }
+    }
+    
+    public boolean contains(int key) {
+        return array.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
