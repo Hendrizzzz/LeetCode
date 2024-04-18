675453f7f22a423b2cd2f04cefd6e88b186bd4d3
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-04-18 20:43:13 +0800

    Time: 1 ms (95.73%), Space: 45.5 MB (47.41%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,23 +1,19 @@
 class Solution {
     
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        ArrayList<Integer> stack = new ArrayList<>(20);
-        stack.add(0);
-        for (int flower : flowerbed){
-            stack.add(flower);
-        }
-        stack.add(0);
-        
-        for (int i = 1; i < stack.size() - 1; i++){
+        for (int i = 0; i < flowerbed.length; i++){
             if (n == 0){
-                return true;
-            }
-            
-            if (stack.get(i - 1) == 0 && stack.get(i) == 0 && stack.get(i + 1) == 0){
-                stack.set(i, 1);
+                    return true;
+                }
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) ){
                 n--;
+                if (n == 0){
+                    return true;
+                }
+                flowerbed[i] = 1;
                 i++;
             }
+         
         }
 
         return (n == 0);
